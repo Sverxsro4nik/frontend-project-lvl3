f29d5f9bd2e148d0eedb8948b87ac10c934a687e
Dmitriu Volchenko <Sverxsro4nik@gmail.com>
2022-04-25 15:08:01 +0500

    create first structure, add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    compress: false,
+    port: 9000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+};
